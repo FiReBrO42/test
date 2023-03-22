02768e9a078e4080ee0ab826296c62c56340c100
Sam Wang <sam1246747@hotmail.com>
2023-03-22 23:38:22 +0800

    Update all.js

--- js/all.js
+++ js/all.js
@@ -58,31 +58,31 @@ Vue.createApp({
           title: '黑小丑',
           imageUrl: '../img/04_Games/img_game_bg.jpg',
           imageAlt: '遊戲機台圖',
-          link: '../convertPoints.html',
+          link: '/convertPoints.html',
           number: 'H238-1:2'
         },
         {
           category: 'available',
           title: '黑小丑',
-          imageUrl: '../img/04_Games/img_game_bg.jpg',
+          imageUrl: '/img/04_Games/img_game_bg.jpg',
           imageAlt: '遊戲機台圖',
-          link: '../convertPoints.html',
+          link: '/convertPoints.html',
           number: 'H238-1:2'
         },
         {
           category: 'gaming',
           title: '黑小丑',
-          imageUrl: '../img/04_Games/img_game_bg.jpg',
+          imageUrl: '/img/04_Games/img_game_bg.jpg',
           imageAlt: '遊戲機台圖',
-          link: '../convertPoints.html',
+          link: '/convertPoints.html',
           number: 'H238-1:2'
         },
         {
           category: 'gaming',
           title: '黑小丑',
-          imageUrl: '../img/04_Games/img_game_bg.jpg',
+          imageUrl: '/img/04_Games/img_game_bg.jpg',
           imageAlt: '遊戲機台圖',
-          link: '../convertPoints.html',
+          link: '/convertPoints.html',
           number: 'H238-1:2'
         }
 
